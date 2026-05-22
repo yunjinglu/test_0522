c44b27bb214ad5cdc4beab90e277b37be80c7fda
yunjinglu <yunjinglu3@gmail.com>
2026-05-22 11:45:25 +0800

    fix API

--- src/calculate.py
+++ src/calculate.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+
 app = FastAPI()
+
 
 def add_fun(a, b):
     return a + b
@@ -14,11 +16,13 @@ def mul_fun(a: float, b: float) -> float:
     # return 123
     return a * b
 
+
 @app.get("/")
 def home():
-    return {"status": "Online", "message" : "這是簡易計算機API"}
+    return {"status": "Online", "message": "這是簡易計算機API"}
+
 
 @app.get("/add")
 def add(a: float, b: float):
     result = add_fun(a, b)
-    return {"operation" : "addition", "a": a, "b": b, "result": result}
+    return {"operation": "addition", "a": a, "b": b, "result": result}
